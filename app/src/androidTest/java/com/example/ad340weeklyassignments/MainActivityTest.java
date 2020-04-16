8e37f33c6ebc900cfd8b02fbe087d103b85a83fe
kevin <kevinpettinger@gmail.com>
2020-04-16 15:45:55 -0700

    Finished first assignment. Name, date, hello world, and dog background. Added Testing

---
+++ app/src/androidTest/java/com/example/ad340weeklyassignments/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.ad340weeklyassignments;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void hasTextOnScreen() {
+        onView(withId(R.id.helloworld))
+                .check(matches(withText(R.string.hello_world)));
+
+    }
+
+    @Test
+    public void hasNameOnScreen() {
+        onView(withId(R.id.name))
+                .check(matches(withText("kevin pettinger")));
+    }
+}
