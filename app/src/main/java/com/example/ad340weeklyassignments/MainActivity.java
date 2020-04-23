639fa2fe39d1a9e77f3ec5c7fc26c77ab311c62c
kevin <kevinpettinger@gmail.com>
2020-04-22 19:17:42 -0700

    created form checker except for birthday

--- app/src/main/java/com/example/ad340weeklyassignments/MainActivity.java
+++ app/src/main/java/com/example/ad340weeklyassignments/MainActivity.java
@@ -8,11 +8,14 @@ import android.content.Intent;
 import android.media.audiofx.DynamicsProcessing;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
@@ -50,7 +53,30 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     }
 
     public void onSubmit(View view) {
+        String testName = name.getText().toString();
+        String testEmail = email.getText().toString();
+        String testUsername = username.getText().toString();
+        int testAge = Integer.parseInt(age.getText().toString());
+        String bday = dob.getText().toString();
 
+        ArrayList<String> errors = new ArrayList<>();
+
+        if (testName.equals(""))
+            errors.add("name");
+        if (testUsername.equals(""))
+            errors.add("username");
+        if (testAge < 18)
+            errors.add("age");
+        if (!testEmail.matches("^(.+)@(.+)$"))
+            errors.add("email");
+
+
+        if (errors.size() > 0)
+            goToFail(errors);
+
+    }
+
+    public void goToSuccess() {
         Intent intent = new Intent(MainActivity.this, SubmitSuccess.class);
         Bundle userInfo = new Bundle();
         userInfo.putString(Constants.KEY_NAME, name.getText().toString());
@@ -60,5 +86,9 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         userInfo.putString(Constants.KEY_DOB, dob.getText().toString());
         intent.putExtras(userInfo);
         startActivity(intent);
+    }
+
+    public void goToFail(ArrayList<String> errors) {
+
     }
 }
--- app/src/main/java/com/example/ad340weeklyassignments/SubmitFail.java
+++ app/src/main/java/com/example/ad340weeklyassignments/SubmitFail.java
@@ -2,7 +2,9 @@ package com.example.ad340weeklyassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubmitFail extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SubmitFail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_fail);
+        TextView failMsg = findViewById(R.id.failMsg);
+        Intent intent = getIntent();
+
+        String msg = intent.getStringExtra("fail");
+
+        failMsg.setText(msg);
     }
 }
